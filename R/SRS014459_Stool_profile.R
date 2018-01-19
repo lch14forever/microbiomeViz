ed8aaa4e02eca155f1e69d4ccc6aa4af0d7c516f
Chenhao Li <lichenhao.sg@gmail.com>
2018-01-19 16:57:56 +0800

    add sample data

---
+++ R/SRS014459_Stool_profile.R
@@ -0,0 +1,12 @@
+#'
+#'
+#' A sample metaphlan table
+#'
+#' @format A data frame with 33 rows and 2 variables:
+#' \describe{
+#'   \item{V1}{Taxonomic annotation}
+#'   \item{V2}{Relative abundances}
+#'   ...
+#' }
+#' @source \url{https://bitbucket.org/biobakery/biobakery/raw/tip/demos/biobakery_demos/data/metaphlan2/output/SRS014459-Stool_profile.txt}
+"SRS014459_Stool_profile"
--- R/parser.R
+++ R/parser.R
@@ -10,7 +10,7 @@
 
 ##' @title parseMetaphlanTSV
 ##'
-##' @param file a single metaphlan table
+##' @param tax.profile a single metaphlan table (either a file path or a a data.frame)
 ##' @param delimiter delimiter to separate taxonomic anotations
 ##' @param index the column number of taxonomic annotation
 ##' @param node.size.scale the parameter 'a' controlling node size: nodeSize=a*log(relative_abundance)+b
@@ -23,11 +23,17 @@
 ##' @author Chenhao Li, Guangchuang Yu
 ##' @export
 ##' @description parse a MetaPhlan table to a tree object
-parseMetaphlanTSV <- function(file, index=1, delimiter='\\|', node.size.scale=1, node.size.offset=1){
-    taxtab <- read.table(file, sep='\t', stringsAsFactors=FALSE) %>%
-        slice(-grep('unclassified', .[,index]))
+parseMetaphlanTSV <- function(tax.profile, index=1, delimiter='\\|', node.size.scale=1, node.size.offset=1){
+    if (is.character(tax.profile)) {
+        taxtab <- read.table(tax.profile, sep='\t', stringsAsFactors=FALSE)
+    }else{
+        taxtab <- tax.profile
+    }
+    names(taxtab)[index] <- 'tax'
+    names(taxtab)[-index] <- 'rel_abun'
+    taxtab <- taxtab %>% slice(-grep('unclassified', .[,index]))
     tax_chars <- c('k', 'p', 'c', 'o', 'f', 'g', 's', 't')
-    tax_split <- strsplit(taxtab$V1, delimiter)    ## split into different taxonomy levels
+    tax_split <- strsplit(taxtab$tax, delimiter)    ## split into different taxonomy levels
     child <- vapply(tax_split, tail, n=1, '')
     tax_class <- do.call(rbind, strsplit(child, '__'))[,1]
     parent <- vapply(tax_split, function(x) ifelse(length(x)>1, x[length(x)-1], 'root'), '')
@@ -36,7 +42,7 @@ parseMetaphlanTSV <- function(file, index=1, delimiter='\\|', node.size.scale=1,
     index[isTip] <- 1:sum(isTip)
     index[!isTip] <- (sum(isTip)+1):length(isTip)
     ## tips comes first
-    mapping <- data.frame(node=index, row.names=child, isTip, taxaAbun=taxtab$V2)
+    mapping <- data.frame(node=index, row.names=child, isTip, taxaAbun=taxtab$rel_abun)
     edges <- cbind(mapping[parent,]$node, mapping$node)
     edges <- edges[!is.na(edges[,1]),]
 
